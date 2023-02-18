90f4d100f9ac5f120cc6d6552c48c3fda7d3bf6f
Orwa Diraneyya <107997002+diraneyya@users.noreply.github.com>
2023-02-18 17:46:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { createElement } from 'react';
+
+function Greeting({ name }) {
+  return createElement(
+    'h1',
+    { className: 'greeting' },
+    'Hello ',
+    createElement('i', null, name),
+    '. Welcome!'
+  );
+}
+
+export default function App() {
+  return createElement(
+    Greeting,
+    { name: 'Taylor' }
+  );
+}
